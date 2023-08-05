d3dee6c88c4035f858a617991c528ef63c126bf1
wgsxm <2901199020@qq.com>
2023-08-06 02:04:50 +0800

    finished Project1 gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -22,32 +22,30 @@ public class TestArrayDequeGold {
             }
         }
     }
-    @Test
-    public void testStudentArrayDeque(){
-        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
-        int size=0;
-        String message="";
-        String check="";
-        while(true)
-        {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-            int randomNumber = StdRandom.uniform(0,10);
 
-            if(numberBetweenZeroAndOne<0.5)
-            {
-                message+="addFirst("+(char)('0'+randomNumber)+")\n";
-                check+=(char)('0'+randomNumber);
+    @Test
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        int size = 0;
+        String message = "";
+        String check = "";
+        while (true) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            int randomNumber = StdRandom.uniform(0, 10);
+
+            if (numberBetweenZeroAndOne < 0.5) {
+                message += "addFirst(" + (char) ('0' + randomNumber) + ")\n";
+                check += (char) ('0' + randomNumber);
                 size++;
                 sad.addFirst(randomNumber);
-            }
-            else {
-                if(size>0){
+            } else {
+                if (size > 0) {
                     size--;
-                    int answer=check.charAt(0)-'0';
-                    check=check.substring(1);
-                    message+="removeFirst()\n";
-                    int temp=sad.removeFirst();
-                    assertEquals(message,answer,temp);
+                    int answer = check.charAt(0) - '0';
+                    check = check.substring(1);
+                    message += "removeFirst()\n";
+                    int temp = sad.removeFirst();
+                    assertEquals(message, answer, temp);
                 }
             }
         }
