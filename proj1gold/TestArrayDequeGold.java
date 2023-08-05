f89a93e18f4a9764d1a15333c4046655e1348ef9
wgsxm <2901199020@qq.com>
2023-08-06 02:37:06 +0800

    a try to fix Project1 B

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -8,25 +8,31 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
         String message = "";
-        int size = 0;
         for (int i = 0; ; i++) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
-            if (numberBetweenZeroAndOne < 0.5) {
-                message += "addFirst(" + (char) ('0' + i) + ")\n";
+            if (numberBetweenZeroAndOne < 0.25) {
+                message += "addFirst(" +  i + ")\n";
                 sad1.addFirst(i);
                 sad2.addFirst(i);
-                size++;
-            } else if (numberBetweenZeroAndOne > 0.5) {
-                if (size > 0) {
-                    int a = sad1.removeFirst();
-                    int b = sad2.removeFirst();
+                assertEquals(message,sad1.get(0),sad2.get(0));
+            }else if(numberBetweenZeroAndOne>0.25&&numberBetweenZeroAndOne<0.5){
+                message += "addLast(" +  i + ")\n";
+                sad1.addLast(i);
+                sad2.addLast(i);
+                assertEquals(message,sad1.get(sad1.size()-1),sad2.get(sad2.size()-1));
+            }else if(numberBetweenZeroAndOne>0.5&&numberBetweenZeroAndOne<0.75){
+                if (sad1.size() > 0) {
+                    message += "removeLast()\n";
+                    assertEquals(message, (int) sad1.removeLast(), (int) sad2.removeLast());
+                }
+            }
+            else if (numberBetweenZeroAndOne > 0.75) {
+                if (sad1.size() > 0) {
                     message += "removeFirst()\n";
-                    assertEquals(message, a, b);
-                    size--;
+                    assertEquals(message, (int) sad1.removeFirst(), (int) sad2.removeFirst());
                 }
             }
         }
     }
-
 
 }
