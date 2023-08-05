8a409517b84dd5fdbefc1d63410da0f05b42d858
wgsxm <2901199020@qq.com>
2023-08-06 02:17:29 +0800

    fixed Project1 gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,47 +7,26 @@ public class TestArrayDequeGold {
     public void someTest() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
+        String message = "";
         int size = 0;
         for (int i = 0; ; i++) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
             if (numberBetweenZeroAndOne < 0.5) {
+                message += "addFirst(" + (char) ('0' + i) + ")\n";
                 sad1.addFirst(i);
                 sad2.addFirst(i);
                 size++;
-            } else if (numberBetweenZeroAndOne > 0.75) {
+            } else if (numberBetweenZeroAndOne > 0.5) {
                 if (size > 0) {
-                    assertEquals((int) sad1.removeFirst(), (int) sad2.removeLast());
-                    size--;
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testStudentArrayDeque() {
-        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
-        int size = 0;
-        String message = "";
-        String check = "";
-        while (true) {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-            int randomNumber = StdRandom.uniform(0, 10);
-
-            if (numberBetweenZeroAndOne < 0.5) {
-                message += "addFirst(" + (char) ('0' + randomNumber) + ")\n";
-                check += (char) ('0' + randomNumber);
-                size++;
-                sad.addFirst(randomNumber);
-            } else {
-                if (size > 0) {
-                    size--;
-                    int answer = check.charAt(0) - '0';
-                    check = check.substring(1);
+                    int a = sad1.removeFirst();
+                    int b = sad2.removeFirst();
                     message += "removeFirst()\n";
-                    int temp = sad.removeFirst();
-                    assertEquals(message, answer, temp);
+                    assertEquals(message, a, b);
+                    size--;
                 }
             }
         }
     }
+
+
 }
